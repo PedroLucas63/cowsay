0aae7a32aee7af2a012a13ea3bc03632097760b8
Pedro Lucas <pedrolucas.jsrn@gmail.com>
2023-09-15 15:36:30 -0300

    ✨ Added main code

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
-// #include "clioptions.hpp"
+#include "clioptions.hpp"
 //#include "drawing.hpp"
 
 int main(int argc, char* argv[]) {
-   
+   CLIOptions& cli { CLIOptions::getInstance() };
+   cli.getConfigs(argc, argv);
+
+   if (cli.getExecute()) {
+      cli.getPendingConfig();
+   }
+
    return 0;
 }
