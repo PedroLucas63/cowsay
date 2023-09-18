85f17b69d152dea1bc148ca3ca6dbfc21d4a7d72
Pedro Lucas <pedrolucas.jsrn@gmail.com>
2023-09-18 10:56:28 -0300

    🔧 Updated utilities

--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -12,9 +12,29 @@
  */
 
 #include <string>
+#include "fstring.hpp"
 
 #ifndef UTILS_HPP_
 #define UTILS_HPP_
+
+enum Moods {
+   DEFAULT,
+   BORG,
+   DEAD,
+   GREEDY,
+   PARANOIA,
+   STONED,
+   TIRED,
+   WIRED,
+   YOUNG,
+};
+
+constexpr bool DEFAULT_REGULAR { false };
+constexpr size_t DEFAULT_COLUMN_SIZE { 40 };
+constexpr fos::align DEFAULT_ALIGN { fos::align::left };
+
+constexpr char* DEFAULT_CREATURE { "./data/default.cow" };
+constexpr Moods DEFAULT_MOOD { Moods::DEFAULT };
 
 /**
  * @brief Structure that defines configurations for a billboard
@@ -23,10 +43,10 @@
  */
 struct BillboardConfigs {
    std::string text; ///< The text to be displayed on the billboard
-   bool regular; ///< Specifies whether the text is regular (true) or special
+   bool regular { DEFAULT_REGULAR }; ///< Specifies whether the text is regular (true) or special
                  ///< (false)
-   int wrap_column; ///< The column limit for automatic line breaks
-   char align; ///< The alignment of the text (e.g., 'l' for left, 'c' for
+   size_t column_size { DEFAULT_COLUMN_SIZE }; ///< The column limit for automatic line breaks
+   fos::align align { DEFAULT_ALIGN }; ///< The alignment of the text (e.g., 'l' for left, 'c' for
                ///< centered)
 };
 
@@ -37,8 +57,8 @@ struct BillboardConfigs {
  * features
  */
 struct CreatureConfigs {
-   std::string creature; ///< The file of the creature
-   char mood; ///< The mood of the creature (e.g., 'b' for borg, 'd' for dead).
+   std::string creature { DEFAULT_CREATURE }; ///< The file of the creature
+   Moods mood { DEFAULT_MOOD }; ///< The mood of the creature (e.g., 'b' for borg, 'd' for dead).
    std::string eyes; ///< The appearance of the creature's eyes
    std::string tongue; ///< The appearance of the creature's tongue
 };
