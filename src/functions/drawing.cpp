81b050df3db5de93fc4d3b725b83a98ea6fa8e7b
Pedro Lucas <pedrolucas.jsrn@gmail.com>
2023-09-20 16:58:40 -0300

    ✨ Added drawing functions

--- src/functions/drawing.cpp
+++ src/functions/drawing.cpp
@@ -1,17 +1,44 @@
-#include "drawing.hpp"
+/**
+ * @file drawing.cpp
+ * @author Pedro Lucas (pedrolucas.jsrn@gmail.com)
+ * @brief Implementation file for drawing functions.
+ * @version 0.1
+ * @date 2023-09-20
+ *
+ * This file contains the implementation of drawing functions, including the
+ * main drawing function that creates graphical displays with customizable
+ * settings. It also contains functions for retrieving the creature's design
+ * from a file and selecting the creature's mood.
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
 
-void draw(BillboardConfigs billboard_configs, CreatureConfigs creature_configs, bool flip) {
-   Billboard billboard { billboard_configs.text, billboard_configs.regular, billboard_configs.column_size, billboard_configs.align };
-   billboard.draw();
+#include "drawing.hpp" // Include the drawing functions header.
 
-   std::string creature { getCreature(creature_configs.creature) };
+// Main drawing function that creates graphical displays.
+void draw(BillboardConfigs billboard_configs, CreatureConfigs creature_configs,
+  bool flip) {
+   std::string creature_str { getCreatureFile(creature_configs.creature) };
    selectMood(creature_configs);
-   
-   Creature cre { creature, creature_configs.eyes, creature_configs.tongue, flip };
-   cre.draw();
+
+   Creature creature {
+      creature_str, creature_configs.eyes, creature_configs.tongue, flip
+   };
+
+   size_t width_offset { flip ? fos::longestLine(creature.getCreature()) : 0 };
+   Billboard billboard { billboard_configs.text,
+      billboard_configs.regular,
+      billboard_configs.column_size,
+      billboard_configs.align,
+      width_offset };
+
+   billboard.drawBillboard();
+   creature.drawCreature();
 }
 
-std::string getCreature(std::string local) {
+// Function to retrieve the creature's design from a file.
+std::string getCreatureFile(std::string local) {
    std::ifstream file;
    file.open(local);
 
@@ -28,10 +55,10 @@ std::string getCreature(std::string local) {
       if (fos::subString(line, init_comments.size()) == INIT_COMMENTS) {
          continue;
       } else if (line == INIT_CREATURE) {
-get_creature = true;
+         get_creature = true;
       } else if (line == END_CREATURE) {
          get_creature = false;
-      } else {
+      } else if (get_creature) {
          creature += line + '\n';
       }
    }
@@ -39,12 +66,38 @@ get_creature = true;
    return creature;
 }
 
+// Function to select the creature's mood based on configurations.
 void selectMood(CreatureConfigs& creature_configs) {
-   if (creature_configs.eyes.empty()) {
-   creature_configs.eyes = "00";
-   }
-
+   if (creature_configs.mood == DEFAULT) {
+      if (creature_configs.eyes.empty()) {
+         creature_configs.eyes = "oo";
+      }
       if (creature_configs.tongue.empty()) {
-   creature_configs.tongue = " U";
+         creature_configs.tongue = "  ";
+      }
+   } else if (creature_configs.mood == BORG) {
+      creature_configs.eyes = "==";
+      creature_configs.tongue = "  ";
+   } else if (creature_configs.mood == DEAD) {
+      creature_configs.eyes = "xx";
+      creature_configs.tongue = " U";
+   } else if (creature_configs.mood == GREEDY) {
+      creature_configs.eyes = "$$";
+      creature_configs.tongue = "  ";
+   } else if (creature_configs.mood == PARANOIA) {
+      creature_configs.eyes = "@@";
+      creature_configs.tongue = "  ";
+   } else if (creature_configs.mood == STONED) {
+      creature_configs.eyes = "**";
+      creature_configs.tongue = " U";
+   } else if (creature_configs.mood == TIRED) {
+      creature_configs.eyes = "--";
+      creature_configs.tongue = "  ";
+   } else if (creature_configs.mood == WIRED) {
+      creature_configs.eyes = "OO";
+      creature_configs.tongue = "  ";
+   } else if (creature_configs.mood == YOUNG) {
+      creature_configs.eyes = "..";
+      creature_configs.tongue = "  ";
    }
 }
--- src/functions/drawing.hpp
+++ src/functions/drawing.hpp
@@ -1,18 +1,67 @@
-#include "utils.hpp"
-#include "billboard.hpp"
-#include "creature.hpp"
-#include <fstream>
-#include <string> 
+/**
+ * @file drawing.hpp
+ * @author Pedro Lucas (pedrolucas.jsrn@gmail.com)
+ * @brief Header file for the drawing functions.
+ * @version 0.1
+ * @date 2023-09-20
+ *
+ * This file defines functions related to drawing, including functions to create
+ * graphical displays with customizable settings and select mood for a creature.
+ * It also contains constants related to drawing initialization and creature
+ * representation.
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
 
-#ifndef DRAWING_HPP_
+#include "billboard.hpp" // Include the Billboard class header
+#include "creature.hpp" // Include the Creature class header
+#include "utils.hpp" // Include utility functions and constants
+#include <fstream> // Include file stream functionality
+#include <string> // Include string manipulation functions
+
+#ifndef DRAWING_HPP_ // Include guard to prevent multiple inclusions
 #define DRAWING_HPP_
 
-constexpr char* INIT_COMMENTS { "##" };
-constexpr char* INIT_CREATURE { "$the_creature = <<EOC;" };
-constexpr char* END_CREATURE { "EOC" };
+constexpr const char* INIT_COMMENTS { "##" }; ///< Initial marker for comments
+constexpr const char* INIT_CREATURE {
+   "$the_creature = <<EOC;"
+}; ///< Initial marker for creature
+constexpr const char* END_CREATURE { "EOC" }; ///< End marker for creature
 
-void draw(BillboardConfigs billboard_configs, CreatureConfigs creature_configs, bool flip);
-std::string getCreature(std::string local);
+/**
+ * @brief Draw a graphical display with customizable settings
+ *
+ * This function takes BillboardConfigs and CreatureConfigs as input to create a
+ * graphical display with the specified settings. The 'flip' parameter
+ * determines whether to flip the creature's design.
+ *
+ * @param billboard_configs Configurations for the billboard display
+ * @param creature_configs Configurations for the creature's appearance
+ * @param flip Indicates whether to flip the creature's design
+ */
+void draw(BillboardConfigs billboard_configs, CreatureConfigs creature_configs,
+  bool flip);
+
+/**
+ * @brief Retrieve the file content representing the creature's design
+ *
+ * This function retrieves the content of the file that represents the
+ * creature's design based on the 'local' parameter.
+ *
+ * @param local The location of the creature's design file
+ * @return A string containing the creature's design
+ */
+std::string getCreatureFile(std::string local);
+
+/**
+ * @brief Select a mood for the creature
+ *
+ * This function allows you to select a mood for the creature by modifying the
+ * CreatureConfigs structure.
+ *
+ * @param creature_configs The configuration structure for the creature
+ */
 void selectMood(CreatureConfigs& creature_configs);
 
 #endif // DRAWING_HPP_
