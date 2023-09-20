bada2c077684a6fffd403bc1dfa440e7d8f9f462
Pedro Lucas <pedrolucas.jsrn@gmail.com>
2023-09-20 16:51:16 -0300

    ✨ Added billboard functions

--- src/classes/billboard/billboard.cpp
+++ src/classes/billboard/billboard.cpp
@@ -1,39 +1,66 @@
+/**
+ * @file billboard.cpp
+ * @author Pedro Lucas (pedrolucas.jsrn@gmail.com)
+ * @brief Implementation file for the Billboard class.
+ * @version 0.1
+ * @date 2023-09-20
+ *
+ * This file contains the implementation of the Billboard class, which
+ * represents a graphical text display with customizable settings such as text
+ * content, regular or special formatting, column size, alignment, and width
+ * offset.
+ *
+ * @copyright Copyright (c) 2023
+ */
+
 #include "billboard.hpp"
 
-Billboard::Billboard(std::string text_, bool regular_, size_t column_size_, fos::align alignment_) {
+// Constructor for the Billboard class
+Billboard::Billboard(std::string text_, bool regular_, size_t column_size_,
+  fos::align alignment_, size_t width_offset_) {
    text = text_;
    regular = regular_;
    column_size = column_size_;
    alignment = alignment_;
+   width_offset = width_offset_;
 
-   format();
+   formatText();
 }
 
-void Billboard::draw() {
+// Draw the graphical text display on the billboard
+void Billboard::drawBillboard() {
    std::vector<std::string> lines { fos::split(text, "\n") };
    size_t max_line_size { fos::longestLine(text) };
    auto last { lines.end() - 1 };
 
-   std::cout << ' ' << fos::repeat('-', max_line_size + 2) << '\n';
+   // Draw the top border of the billboard
+   std::cout << fos::repeat(' ', width_offset) << ' '
+             << fos::repeat('_', max_line_size + 2) << '\n';
 
+   // Iterate through lines and draw the content
    for (auto line { lines.begin() }; line < lines.end(); ++line) {
       if (line == lines.begin() && line == last) {
-         std::cout << "< " << *line << " >\n";
+         std::cout << fos::repeat(' ', width_offset) << "< " << *line << " >\n";
       } else if (line == lines.begin()) {
-         std::cout << "/ " << *line << " \\\n";
+         std::cout << fos::repeat(' ', width_offset) << "/ " << *line
+                   << " \\\n";
       } else if (line == last) {
-         std::cout << "\\ " << *line << " /\n";
+         std::cout << fos::repeat(' ', width_offset) << "\\ " << *line
+                   << " /\n";
       } else {
-         std::cout << "| " << *line << " |\n";
+         std::cout << fos::repeat(' ', width_offset) << "| " << *line << " |\n";
       }
    }
 
-   std::cout << ' ' << fos::repeat('-', max_line_size + 2) << '\n';
+   // Draw the bottom border of the billboard
+   std::cout << fos::repeat(' ', width_offset) << ' '
+             << fos::repeat('-', max_line_size + 2) << '\n';
 }
 
-void Billboard::format() {
+// Format the text on the billboard based on the specified settings
+void Billboard::formatText() {
    if (!regular) {
       text = fos::columnWrap(text, column_size);
-      text = fos::justify(text, column_size, alignment, ' ', true, true);
+      text = fos::alignment(text, column_size, alignment, ' ', true, true);
    }
 }
--- src/classes/billboard/billboard.hpp
+++ src/classes/billboard/billboard.hpp
@@ -1,23 +1,57 @@
-#include <iostream>
-#include <vector>
-#include <string>
+/**
+ * @file billboard.hpp
+ * @author Pedro Lucas (pedrolucas.jsrn@gmail.com)
+ * @brief Header file for the Billboard class, representing a graphical text
+ * display.
+ * @version 0.1
+ * @date 2023-09-20
+ *
+ * This file defines the Billboard class, which allows you to create and draw
+ * graphical text displays with customizable settings such as text content,
+ * regular or special formatting, column size, alignment, and width offset.
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
 #include "fstring.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
 
 #ifndef BILLBOARD_HPP_
 #define BILLBOARD_HPP_
 
 class Billboard {
-public:
-Billboard(std::string text_, bool regular_, size_t column_size_, fos::align alignment_);
-void draw();
+   public:
+   /**
+    * @brief Construct a new Billboard object with specified settings
+    *
+    * @param text_ The text to be displayed on the billboard
+    * @param regular_ Specifies whether the text is regular (true) or special (false)
+    * @param column_size_ The column limit for automatic line breaks
+    * @param alignment_ The alignment of the text (e.g., 'l' for left, 'c' for centered)
+    * @param width_offset_ The width offset for adjusting the display width
+    */
+   Billboard(std::string text_, bool regular_, size_t column_size_,
+     fos::align alignment_, size_t width_offset_);
 
-private:
-std::string text;
-bool regular;
-size_t column_size;
-fos::align alignment;
+   /**
+    * @brief Draw the graphical text display on the billboard
+    */
+   void drawBillboard();
 
-void format();
+   private:
+   std::string text; ///< The text content to be displayed on the billboard
+   bool regular; ///< Specifies whether the text is regular (true) or special (false)
+   size_t column_size; ///< The column limit for automatic line breaks
+   fos::align alignment; ///< The alignment of the text (e.g., 'l' for left, 'c' for centered)
+   size_t width_offset; ///< The width offset for adjusting the display width
+
+   /**
+    * @brief Format and display the text on the billboard based on the specified settings
+    */
+   void formatText();
 };
 
 #endif // BILLBOARD_HPP_
