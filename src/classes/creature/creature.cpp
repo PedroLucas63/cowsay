18da7c9dffb402767f8e3a036d9837ec6e1b0b7b
Pedro Lucas <pedrolucas.jsrn@gmail.com>
2023-09-20 16:05:41 -0300

    ✨ Added creature definition

--- src/classes/creature/creature.cpp
+++ src/classes/creature/creature.cpp
@@ -1,21 +1,53 @@
+/**
+ * @file creature.fpp
+ * @author Pedro Lucas (pedrolucas.jsrn@gmail.com)
+ * @brief Implementation file for the Creature class.
+ * @version 0.1
+ * @date 2023-09-20
+ *
+ * This file contains the implementation of the Creature class, which represents
+ * a creature with customizable features including body, eyes, and tongue. The
+ * class provides methods to draw the creature and replace special directives
+ * within its design.
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
 #include "creature.hpp"
 
-Creature::Creature(std::string body_, std::string eyes_, std::string tongue_, bool flip_) {
+// Constructor for the Creature class.
+Creature::Creature(
+  std::string body_, std::string eyes_, std::string tongue_, bool flip_) {
    body = body_;
    eyes = eyes_;
    tongue = tongue_;
    flip = flip_;
-
-   replaces();
 }
 
+// Draw the creature's design, optionally flipping it.
 void Creature::draw() {
-   std::cout << creature;
+   replaceDirectives();
+
+   if (!flip) {
+      std::cout << creature;
+   } else {
+      std::vector<std::string> lines { fos::split(creature, "\n") };
+      for (std::string line : lines) {
+         std::cout << fos::reverse(line) << '\n';
+      }
+   }
 }
 
-void Creature::replaces() {
-   std::string thoughts { flip ? FLIP_THOUGHTS : THOUGHTS };
-   creature = fos::replace(body, TAG_THOUGHTS, thoughts);
+// Replace special directives within the creature's design.
+void Creature::replaceDirectives() {
+   creature = fos::replace(body, TAG_THOUGHTS, THOUGHTS);
    creature = fos::replace(creature, TAG_EYES, eyes);
    creature = fos::replace(creature, TAG_TONGUE, tongue);
+
+   creature = fos::completeLines(creature) + "\n";
+
+   if (flip) {
+      creature = fos::invertCharacters(creature);
+   }
 }
--- src/classes/creature/creature.hpp
+++ src/classes/creature/creature.hpp
@@ -1,28 +1,72 @@
-#include <string>
-#include <iostream>
+/**
+ * @file creature.hpp
+ * @author Pedro Lucas (pedrolucas.jsrn@gmail.com)
+ * @brief Header file for the Creature class, which represents a creature with
+ * customizable features.
+ * @version 0.1
+ * @date 2023-09-20
+ *
+ * This file defines the Creature class, which allows you to create and draw
+ * creatures with customizable body, eyes, and tongue. The class provides
+ * methods to draw the creature and replace special directives within the
+ * creature's design
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
 #include "fstring.hpp"
+#include <iostream>
+#include <string>
 
 #ifndef CREATURE_HPP_
 #define CREATURE_HPP_
 
-constexpr char* THOUGHTS { "/" };
-constexpr char* FLIP_THOUGHTS { "\\" };
-constexpr char* TAG_THOUGHTS { "$thoughts" };
-constexpr char* TAG_EYES { "$eyes" };
-constexpr char* TAG_TONGUE { "$tongue" };
+constexpr const char* THOUGHTS {
+   "\\"
+}; ///< The character representing thoughts in the creature's design
+constexpr const char* TAG_THOUGHTS {
+   "$thoughts"
+}; ///< The tag for thoughts in the creature's design
+constexpr const char* TAG_EYES {
+   "$eyes"
+}; ///< The tag for eyes in the creature's design
+constexpr const char* TAG_TONGUE {
+   "$tongue"
+}; ///< The tag for tongue in the creature's design
 
 class Creature {
-public:
-Creature(std::string body_, std::string eyes_, std::string tongue_, bool flip_);
-void draw();
-private:
-std::string body;
-std::string eyes;
-std::string tongue;
-bool flip;
-std::string creature;
+   public:
+   /**
+    * @brief Construct a new Creature object
+    *
+    * @param body_ The body of the creature
+    * @param eyes_ The eyes of the creature
+    * @param tongue_ The tongue of the creature
+    * @param flip_ Indicates whether to flip the creature's design
+    */
+   Creature(
+     std::string body_, std::string eyes_, std::string tongue_, bool flip_);
 
-void replaces();
+   /**
+    * @brief Draw the creature's design
+    */
+   void draw();
+
+   private:
+   std::string body; ///< The body design of the creature
+   std::string eyes; ///< The eyes design of the creature
+   std::string tongue; ///< The tongue design of the creature
+   bool flip; ///< Indicates whether the creature's design should be flipped
+   std::string creature; ///< The complete creature's design
+
+   /**
+    * @brief Replace special directives within the creature's design
+    *
+    * This method replaces special directives like "$thoughts", "$eyes", and
+    * "$tongue" in the creature's design with their corresponding values
+    */
+   void replaceDirectives();
 };
 
 #endif // CREATURE_HPP_
